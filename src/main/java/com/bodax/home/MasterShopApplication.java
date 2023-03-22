3fd69b5d8354826efbb61e47b28c019f9577c8e8
bodax <bodax@DESKTOP-A2BI7RB>
2023-03-22 20:21:30 +0100

    Init commit

---
+++ src/main/java/com/bodax/home/MasterShopApplication.java
@@ -0,0 +1,13 @@
+package com.bodax.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterShopApplication.class, args);
+	}
+
+}
